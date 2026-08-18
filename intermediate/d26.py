277a4af65058365030dd81dfa16311dc6b1ca2ad
Melon <68168583+awildmelon@users.noreply.github.com>
2026-08-17 20:47:58 -0700

    Update d26.py

--- intermediate/d26.py
+++ intermediate/d26.py
@@ -7,20 +7,17 @@ import os
 base_path = os.path.dirname(__file__)
 data = pd.read_csv(os.path.join(base_path, "packages", "d26", "nato_phonetic_alphabet.csv"))
 
-# Dictionary made using NATO Phonetic Alphabet CSV file
-# dictionary_comprehension = {new_key: new_value for item in list}
-# new_dict = {new_key: new_value for (key, value) in dict.items() if condition}
 
 phonetic_dict = {row.letter: row.code for _, row in data.iterrows()}
 
-# User inputs a word, which is converted into a list of letters
-# list_comprehension = [new_item for item in list if condition]
+def generate_phonetic(word):
+    try:
+        letters_list = [phonetic_dict[letter] for letter in word]
+    except KeyError as e:
+        print("Sorry, only letters in the alphabet are allowed. Please try again.")
+        generate_phonetic(input("Enter a word: ").upper())
+    else:
+        print(letters_list)
 
 word = input("Enter a word: ").upper()
-letters_list = [letter for letter in word]
-
-# Each letter is replaced with the corresponding phonetic code
-
-phonetic_list = [phonetic_dict[letter] for letter in letters_list if letter in phonetic_dict]
-
-print(phonetic_list)
+generate_phonetic(word)
